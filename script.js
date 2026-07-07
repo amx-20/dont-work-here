d8484199adea942f41bccce79c58722efdefd506
amx-20 <2001ahmedmahmou@gmail.com>
2026-07-07 03:12:07 +0300

    Display API data

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnR_Yzst6ad2MiRsNP5pIweHMd7DAXwesD4AjSPc4PyDqU9o1K-NSny1HSM7bOoKLGFP1npEjLPhwDmybg0a7lkWT6FJEiOrFXR6tI4JyrlJUfZWcyhXjEHMcdRGMLN3JGs2WUrzyXAu7rWpcsVvkkM31952mHM6o_jjFa3uPuuhlllTX1icfsvDOeXFArWp807dAtwvLMANXzjlY8O3LaVsfL59VslZbXB4IV0Bx4MseLeZqV8&lib=MaQxjQjLTcTJSyY71HQPmM36CZDhuSRl7";
-
-const companiesDiv = document.getElementById("companies");
-
 fetch(API_URL)
     .then(response => response.json())
     .then(data => {
 
+        console.log(data);
+
         data.forEach(review => {
+
+            console.log(review);
 
             companiesDiv.innerHTML += `
                 <div class="company-card">
