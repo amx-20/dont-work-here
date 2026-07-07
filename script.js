105ea48f26b62621db3f56193b22a06728ff192b
amx-20 <2001ahmedmahmou@gmail.com>
2026-07-07 03:02:02 +0300

    Display API data

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxoiZ1Toe9nYZvnCTE2tzuaB4kPUmLNJ5mlA_KLrxNh1Yq47WBOlTHmCvdexfndE5u4/exec";
+const API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnS0UmPdi4mHXxfGXDgILA3vZfKF5m5sgI6HpFQ0Aa1_DPI4SGG0VV18gYkZUJtNu--nCpUmHkVlXFZKU5y6UkeOy8KZfGtLBBf_eJ5Fc-Q4YrgPvD5QPCoIoVsHChQgWS4zzi6EUllpsH-DE2zfooHf16fAAFkTkPKJpQhck3GW0l4n_uhKZVhB4IB0Mw6SQ75wHI9LkFoL0IG1TIthxhgJVs6fjxVg7zoVhialvI44cVhXE1M&lib=MaQxjQjLTcTJSyY71HQPmM36CZDhuSRl7";
 
 const companiesDiv = document.getElementById("companies");
 
